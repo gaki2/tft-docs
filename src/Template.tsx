1cc9e3cd864108deb5248b83a9d782b294b57dbb
유병각 <bgyu@miridih.com>
2023-11-20 22:18:06 +0900

    feat: tft-util 15 버전으로 업그레이드

---
+++ src/Template.tsx
@@ -0,0 +1,11 @@
+import {Board10, ChampionBadge10, TraitBadge10} from "tft-utils";
+import React from "react";
+
+export const Template = () => {
+  return (
+    <>
+      <ChampionBadge10 name={'Akali'} lang={'ko'} />
+    </>
+  )
+}
+
