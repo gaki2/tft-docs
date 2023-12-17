c25bbf61d6c8ab6209f7bc810b38b8b89cf09d92
유병각 <bgyu@miridih.com>
2023-12-17 23:04:49 +0900

    feat: png 를 webP 로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import RemixRumble from '@site/static/img/remix_rumble_logo.png';
+import RemixRumble from '@site/static/img/remix_rumble_logo.webp';
 import Head from '@docusaurus/Head';
 
 import styles from './index.module.css';
