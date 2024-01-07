e864038a271360e2f9cde407ce1b2fa88e0c849d
gaki2 <qudrkr0815@gmail.com>
2024-01-07 23:00:16 +0900

    feat: 중간 작업 커밋

--- packages/tft-ui/src/components/editor/utils/search.ts
+++ packages/tft-ui/src/components/editor/utils/search.ts
@@ -2,6 +2,8 @@ type List = {
   indexedName: string;
   indexedChosung: string;
 };
+
+// 입력된 Keyword 와 매칭되는 아이템을 필터해서 리턴하는 함수.
 export const search = <T extends List>(list: T[], keyword: string) => {
   return list.filter((item) => {
     return item.indexedName.includes(keyword) || item.indexedChosung.includes(keyword);
